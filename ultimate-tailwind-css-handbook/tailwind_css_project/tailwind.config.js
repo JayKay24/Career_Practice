00ec343c2785e2faac9b7011d6da2501d992b14a
jameskinyua590@gmail.com <jameskinyua590@gmail.com>
2025-02-24 16:22:02 +0300

    add custom color and spacing
    Signed-off-by: jameskinyua590@gmail.com <jameskinyua590@gmail.com>

--- ultimate-tailwind-css-handbook/tailwind_css_project/tailwind.config.js
+++ ultimate-tailwind-css-handbook/tailwind_css_project/tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'silver': '#C0C0C0',
+        'bronze': '#CD7F32'
+      },
+      spacing: {
+        '101': '32rem',
+        '102': '36rem'
+      }
+    },
   },
   plugins: [],
   darkMode: 'class'
