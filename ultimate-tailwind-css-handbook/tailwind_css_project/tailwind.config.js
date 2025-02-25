9f6fbbee7fcba40340c19c2c8e013fba54cf36f3
jameskinyua590@gmail.com <jameskinyua590@gmail.com>
2025-02-25 23:57:26 +0300

    remove custom configuration
    Signed-off-by: jameskinyua590@gmail.com <jameskinyua590@gmail.com>

--- ultimate-tailwind-css-handbook/tailwind_css_project/tailwind.config.js
+++ ultimate-tailwind-css-handbook/tailwind_css_project/tailwind.config.js
@@ -2,16 +2,7 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {
-      colors: {
-        'silver': '#C0C0C0',
-        'bronze': '#CD7F32'
-      },
-      spacing: {
-        '101': '32rem',
-        '102': '36rem'
-      }
-    },
+    extend: {},
   },
   plugins: [],
   darkMode: 'class'
